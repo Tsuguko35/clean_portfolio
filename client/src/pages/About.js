2497ca69ac3571429ddc7ba14661e33b34eb6fbd
Tsuguko35 <carpio.johnjazpher.dc.3188@gmail.com>
2024-07-15 14:31:21 +0800

    about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/about.css";
+import { Skills } from "../components";
 
 function About() {
   return (
@@ -18,7 +19,29 @@ function About() {
             />
           </div>
         </header>
-        About
+        <div className="title-label">
+          <p className="title">About</p>
+          <p className="label">A little bit about who I am</p>
+        </div>
+        <p className="paragraph">
+          I am a passionate Full Stack Developer with expertise in HTML, CSS,
+          JavaScript, React.js,Node.js, PHP, MySQL, and MongoDB. My journey in
+          web development has equipped me with a robust understanding of both
+          front-end and back-end technologies, enabling me to create dynamic,
+          responsive, and user-friendly web applications. With a strong
+          foundation in front-end technologies, I build intuitive interfaces,
+          while my back-end skills ensure efficient server-side logic and secure
+          data management. I am always eager to learn and stay updated with the
+          latest industry trends, ready to tackle innovative projects and
+          deliver creative solutions.
+        </p>
+        <div className="title-label">
+          <p className="title">Skills</p>
+          <p className="label">Skills and Proficiencies</p>
+        </div>
+
+        {/* Skills Component */}
+        <Skills />
       </div>
     </div>
   );
