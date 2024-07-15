1674a82176eeb11c1f1890951f0df4f1d1176eab
Tsuguko35 <carpio.johnjazpher.dc.3188@gmail.com>
2024-07-15 11:38:58 +0800

    some about not responsive

---
+++ client/src/pages/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "../styles/about.css";
+
+function About() {
+  return (
+    <div id="about" className="about page-container">
+      <div className="wrapper">
+        <header>
+          <div className="text-content">
+            <p>Hello,</p>
+            <p>I'm Jazpher</p>
+            <p>Carpio</p>
+          </div>
+          <div className="image">
+            <img
+              src="https://res.cloudinary.com/dkwgg59ur/image/upload/v1719105225/Portfolio_Files/hmppayoiaarwqdk43ks5.webp"
+              alt="Graduation Picture"
+            />
+          </div>
+        </header>
+        About
+      </div>
+    </div>
+  );
+}
+
+export default About;
